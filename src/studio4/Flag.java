5bc4d0c73664fd24ed061cd5de574135370d78be
cncer <cncer@Samantha.dhcp.wustl.edu>
2024-02-21 17:20:26 -0600

    FLAGGGGG GANGDUMB

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,25 @@
 package studio4;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(104,104,216);
+		StdDraw.filledRectangle(0.5,0.5,0.4,0.2);
+		StdDraw.setPenColor(255,255,255);
+		StdDraw.filledRectangle(0.5, 0.6, 0.4, 0.025);
+		StdDraw.filledRectangle(0.5, 0.4, 0.4, 0.025);
+		StdDraw.filledRectangle(0.3, 0.5, 0.025, 0.2);
+		StdDraw.filledRectangle(0.7, 0.5, 0.025, 0.2);
+		StdDraw.filledCircle(0.5, 0.5, 0.04);
+		StdDraw.filledRectangle(0.5, 0.46, 0.02, 0.015);
+		StdDraw.setPenColor(104,104,216);
+		Font font= new Font("Arial",Font.BOLD,25);
+		StdDraw.setFont(font);
+		StdDraw.text(0.5,0.6,"GANGDUMB");
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,27 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		StdDraw.setPenColor(in.nextInt(), in.nextInt(), in.nextInt());
+		Boolean filled = in.nextBoolean();
+		double p1 = in.nextDouble();
+		double p2 = in.nextDouble();
+		double p3 = in.nextDouble();
+		double p4 = in.nextDouble();
+		if (shapeType.equals("rectangle"))
+		{
+			if(filled == true)
+			{
+				StdDraw.filledRectangle(p1, p2, p3, p4);
+			}
+			else
+			{
+				StdDraw.rectangle(p1, p2, p3, p4);
+			}
+		
+		}
+		
+		
+		
 	}
 }
